95fbf3884ffbf1e2f9d8d2c37d70a0cb5c0f1e2a
Ekaterina Kaiuda <ekaterina@192.168.1.8>
2024-07-20 16:23:25 -0300

    update in 2nd branch mail_search.py

--- mail_search.py
+++ mail_search.py
@@ -3,4 +3,4 @@ from selene import browser, be, have
 
 browser.open('https://ya.ru')
 browser.element('[name="text"]').should(be.blank).type('blablabla2').press_enter()
-browser.element('[id="search-result"]').should(have.no.text('pupupuuu2'))
+browser.element('[id="search-result"]').should(have.no.text('pupu2'))
